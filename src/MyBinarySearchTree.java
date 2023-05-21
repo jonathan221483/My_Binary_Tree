fb46fcb9a62dfbdb27ae5c38165839cd0d3e6203
jonathan221483 <captainberkut@gmail.com>
2023-05-21 19:12:07 +0600

    put method

--- src/MyBinarySearchTree.java
+++ src/MyBinarySearchTree.java
@@ -4,6 +4,7 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> {
     {
         private K key;
         private V val;
+        private int size;
         private Node left,right;
         public Node(K key, V val)
         {
@@ -11,7 +12,22 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> {
             this.val = val;
         }
     }
-    public void put(K key,V val){}
+    private Node put(Node current, K key, V val) {
+        if (current == null) {
+            size++;
+            return new Node(key, val);
+        }
+        if (key.compareTo(current.key) < 0){
+            current.left = put(current.left, key, val);
+        } else{
+            current.right = put(current.right, key, val);
+        }
+        return current;
+    }
+    public void put(K key,V val){
+        root = put(root,key,val);
+    }
+
     public V get(K key){}
     public void delete(K key){}
     public Iterable<K> iterator(){}
