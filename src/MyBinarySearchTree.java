47ce2a76c4e4316439fa65416b68c63b3f4b5fa4
jonathan221483 <captainberkut@gmail.com>
2023-05-21 18:34:45 +0600

    class Node inside BST

--- src/MyBinarySearchTree.java
+++ src/MyBinarySearchTree.java
@@ -1,2 +1,14 @@
 public class MyBinarySearchTree<K extends Comparable<K>,V> {
+    private class Node
+    {
+        private K key;
+        private V val;
+        private Node left,right;
+        public Node(K key, V val)
+        {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
 }
