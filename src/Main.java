e18231d266d698c23cd3cdb8d3258b85e04a8edc
jonathan221483 <captainberkut@gmail.com>
2023-05-21 21:34:28 +0600

    comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         MyBinarySearchTree<Integer,String> tree = new MyBinarySearchTree();
-        tree.put(5,"Ruslan");
+        tree.put(5,"Ruslan"); //adding elements
         tree.put(8,"Daniya");
         tree.put(23,"Alikhan");
         tree.put(12,"Alua");
         tree.put(3,"Elvira");
-        for (MyBinarySearchTree.Pairs elem : tree) {
+        tree.put(7,"Who?");
+        tree.delete(7);//deleting elements
+        for (MyBinarySearchTree.Pairs elem : tree) { //traverse all elements
             System.out.println("key is " + elem.key + " and value is " + elem.val);
         }
     }
--- src/MyBinarySearchTree.java
+++ src/MyBinarySearchTree.java
@@ -1,25 +1,23 @@
 import java.lang.NullPointerException;
-import java.util.List;
 import java.util.Stack;
 import java.util.Iterator;
-import java.util.ArrayList;
 
 public class MyBinarySearchTree<K extends Comparable<K>,V> implements Iterable<MyBinarySearchTree.Pairs> {
-    private Node root;
+    private Node root; //first and main node of binary tree
 
     private class Node {
-        private K key;
-        private V val;
-        private int size = 1;
-        private Node left, right;
+        private K key; //key of node - will be used for comparing
+        private V val; //value of node
+        private int size = 1; //size of binary tree
+        private Node left, right; //children of node
 
-        public Node(K key, V val) {
+        public Node(K key, V val) { //node constructor
             this.key = key;
             this.val = val;
         }
     }
 
-    private Node put(Node pointer, K key, V val) {
+    private Node put(Node pointer, K key, V val) { //recursive method for nodes needed to implement insert method
         if (pointer == null) {
             return new Node(key, val);
         }
@@ -34,12 +32,12 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> implements Iterable<M
 
     public void put(K key, V val) {
         root = put(root, key, val);
-    }
+    } //insert new data into binary tree
 
-    public V get(K key) {
+    public V get(K key) { //get data by a key from binary tree
         return get(root, key).val;
     }
-    private Node get(Node pointer, K key) throws NullPointerException{
+    private Node get(Node pointer, K key) throws NullPointerException{ //recursive method for nodes needed to implement get method
         if (key.compareTo(pointer.key) == 0)
             return pointer;
         else if (key.compareTo(pointer.key) < 0)
@@ -47,11 +45,11 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> implements Iterable<M
         else
             return get(pointer.right, key);
     }
-    public int size(){return root.size;}
-    public void delete(K key) {
+    public int size(){return root.size;} //returns the size of binary tree
+    public void delete(K key) { //deletes data by key from binary tree
         root = delete(root, key, true);
     }
-    public Node delete(Node pointer, K key,boolean size1) {
+    public Node delete(Node pointer, K key,boolean size1) { //recursive method for nodes needed to implement deleting
         if (pointer == null) {
             return null;
         }
@@ -84,24 +82,24 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> implements Iterable<M
             return pointer;
         }
 
-    private Node findMin(Node node) {
+    private Node findMin(Node node) { //returns node with minimum key
         if (node.left == null) {
             return node;
         }
         return findMin(node.left);
     }
-    private Node findMax(Node node){
+    private Node findMax(Node node){ //returns node with maximum key
         if (node.right != null){
             return findMax(node.right);
         }
         return node;
     }
 
-    public Iterator<MyBinarySearchTree.Pairs> iterator(){
+    public Iterator<MyBinarySearchTree.Pairs> iterator(){ //iterator needed to implement in-order traverse
         return new MyIterator();
     }
 
-    private class MyIterator implements Iterator<MyBinarySearchTree.Pairs>{
+    private class MyIterator implements Iterator<MyBinarySearchTree.Pairs>{ //Myiterator class
         private Stack<Pairs> stack;
         public MyIterator(){
             this.stack = new Stack<>();
@@ -115,20 +113,20 @@ public class MyBinarySearchTree<K extends Comparable<K>,V> implements Iterable<M
         public Pairs next() {
             return stack.pop();
         }
-        private void inOrder(Node node){
+        private void inOrder(Node node){ //in-order traverse
             if (node == null){
                 return;
             }
             inOrder(node.right);
-            this.stack.push(new Pairs(node.key, node.val));
+            this.stack.push(new Pairs(node.key, node.val)); //it pushes every element from binary tree into stack in order with their keys
             inOrder(node.left);
         }
     }
 
-    public class Pairs<K, V> {
+    public class Pairs<K, V> { //class for key-value pairs of nodes in binary tree
         public K key;
         public V val;
-        public Pairs(K key, V value) {
+        public Pairs(K key, V value) { //constructor for pairs
             this.key = key;
             this.val = value;
         }
